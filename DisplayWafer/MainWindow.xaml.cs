e4dc6c57cd9d928b086f0fd72aa1893a0620ded9
chimingkuei <chimingkuei@gmail.com>
2024-03-20 23:45:24 +0800

    DisplayWafer

---
+++ DisplayWafer/MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using static Template.BaseLogRecord;
+
+namespace Template
+{
+    #region Config Class
+    public class SerialNumber
+    {
+        [JsonProperty("Parameter1_val")]
+        public string Parameter1_val { get; set; }
+        [JsonProperty("Parameter2_val")]
+        public string Parameter2_val { get; set; }
+    }
+
+    public class Model
+    {
+        [JsonProperty("SerialNumbers")]
+        public SerialNumber SerialNumbers { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("Models")]
+        public List<Model> Models { get; set; }
+    }
+    #endregion
+
+    public class MainViewModel
+    {
+        public ObservableCollection<string> Dies { get; set; }
+
+        public MainViewModel()
+        {
+            Dies = new ObservableCollection<string>();
+            // 模拟晶圆上芯片的位置
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    Dies.Add($"{i},{j}");
+                }
+            }
+
+        }
+    }
+
+    public partial class MainWindow : System.Windows.Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region Config
+        private SerialNumber SerialNumberClass()
+        {
+            SerialNumber serialnumber_ = new SerialNumber
+            {
+                Parameter1_val = Parameter1.Text,
+                Parameter2_val = Parameter2.Text
+            };
+            return serialnumber_;
+        }
+
+        private void LoadConfig(int model, int serialnumber, bool encryption = false)
+        {
+            List<RootObject> Parameter_info = Config.Load(encryption);
+            if (Parameter_info != null)
+            {
+                Parameter1.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter1_val;
+                Parameter2.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter2_val;
+            }
+            else
+            {
+                // 結構:2個Models、Models下在各2個SerialNumbers
+                SerialNumber serialnumber_ = SerialNumberClass();
+                List<Model> models = new List<Model>
+                {
+                    new Model { SerialNumbers = serialnumber_ },
+                    new Model { SerialNumbers = serialnumber_ }
+                };
+                List<RootObject> rootObjects = new List<RootObject>
+                {
+                    new RootObject { Models = models },
+                    new RootObject { Models = models }
+                };
+                Config.InitSave(rootObjects, encryption);
+            }
+        }
+       
+        private void SaveConfig(int model, int serialnumber, bool encryption = false)
+        {
+            Config.Save(model, serialnumber, SerialNumberClass(), encryption);
+        }
+        #endregion
+
+        // 用于在 Visual Tree 中查找指定类型的子元素的辅助方法
+        private T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                    {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void ChangeWaferIC(string index)
+        {
+            ContentPresenter contentPresenter = (ContentPresenter)test.ItemContainerGenerator.ContainerFromItem(index);
+
+            // 检查 ContentPresenter 是否存在
+            if (contentPresenter != null)
+            {
+                // 从 ContentPresenter 中找到 Button
+                Button button = FindVisualChild<Button>(contentPresenter);
+
+                // 检查按钮是否存在
+                if (button != null)
+                {
+                    // 更改按钮的背景颜色
+                    button.Background = Brushes.Red; // 或者其他你想要的颜色
+                }
+            }
+        }
+
+        private void ChangeAllWaferIC()
+        {
+            foreach (var item in test.Items)
+            {
+                // 通过 ItemContainerGenerator 找到每个 ContentPresenter
+                ContentPresenter contentPresenter = (ContentPresenter)test.ItemContainerGenerator.ContainerFromItem(item);
+
+                // 检查 ContentPresenter 是否存在
+                if (contentPresenter != null)
+                {
+                    // 从 ContentPresenter 中找到 Button
+                    Button button = FindVisualChild<Button>(contentPresenter);
+
+                    // 检查按钮是否存在
+                    if (button != null)
+                    {
+                        // 更改按钮的背景颜色
+                        button.Background = Brushes.Red; // 或者其他你想要的颜色
+                    }
+                }
+            }
+
+        }
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig(0, 0);
+        }
+        BaseConfig<RootObject> Config = new BaseConfig<RootObject>();
+        #region Log
+        BaseLogRecord Logger = new BaseLogRecord();
+        //Logger.WriteLog("儲存參數!", LogLevel.General, richTextBoxGeneral);
+        #endregion
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Demo):
+                    {
+                        ChangeWaferIC("15,15");
+                        //ChangeAllWaferIC();
+                        break;
+                    }
+               
+            }
+        }
+        #endregion
+
+
+
+
+
+    }
+}
